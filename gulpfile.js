95806b5dfeb4ca4588a9919c307b455ee80b666a
Vilmar Spies <vilmarspies@gmail.com>
2015-11-26 11:00:56 -0200

    fase 1 modulo AngularJS : CONFIGURANDO O AMBIENTE DE DESENVOLVIMENTO

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,89 @@
 var elixir = require('laravel-elixir'),
 	liveReload = require('gulp-livereload'),
-	clean = require('gulp-clean'),
+	clean = require('rimraf'),
 	gulp = require('gulp');
 
 
-gulp.task('teste',function(){
-	console.log('testa funcionando');
+configCode = {
+	assets_path: './resources/assets',
+	build_path: './public/build/assets'
+};
+
+configCode.bower_path = configCode.assets_path + '/../bower_components';
+
+configCode.build_path_js = configCode.build_path +'/js';
+configCode.build_vendor_path_js = configCode.build_path_js + '/vendor';
+configCode.vendor_path_js = [
+	configCode.bower_path + '/jquery/dist/jquery.min.js',
+	configCode.bower_path + '/bootstrap/dist/js/bootstrap.min.js',
+	configCode.bower_path + '/angular/angular.min.js',
+	configCode.bower_path + '/angular-route/angular-route.min.js',
+	configCode.bower_path + '/angular-resource/angular-resource.min.js',
+	configCode.bower_path + '/angular-animate/angular-animate.min.js',
+	configCode.bower_path + '/angular-messages/angular-messages.min.js',
+	configCode.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
+	configCode.bower_path + '/angular-strap/dist/modules/navbar.min.js'
+];
+
+configCode.build_path_css = configCode.build_path+'/css';
+configCode.build_vendor_path_css = configCode.build_path_css + '/vendor';
+configCode.vendor_path_css = [
+	configCode.bower_path + '/bootstrap/dist/css/bootstrap.min.css',
+	configCode.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css',
+	configCode.bower_path + '/font-awesome/css/font-awesome.min.css',
+];
+
+configCode.build_path_html = configCode.build_path + '/views';
+
+gulp.task('copy-html',function(){
+	gulp.src([configCode.assets_path + '/js/views/**/*.html'])
+		.pipe(gulp.dest(configCode.build_path_html))
+		.pipe(liveReload());
 });
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+gulp.task('copy-styles',function(){
+	gulp.src([configCode.assets_path + '/css/**/*.css'])
+		.pipe(gulp.dest(configCode.build_path_css))
+		.pipe(liveReload());
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+	gulp.src(configCode.vendor_path_css)
+		.pipe(gulp.dest(configCode.build_vendor_path_css))
+		.pipe(liveReload());
 });
+
+gulp.task('copy-scripts', function(){
+	gulp.src([configCode.assets_path + '/js/**/*.js'])
+		.pipe(gulp.dest(configCode.build_path_js))
+		.pipe(liveReload());
+
+	gulp.src(configCode.vendor_path_js)
+		.pipe(gulp.dest(configCode.build_vendor_path_js))
+		.pipe(liveReload());
+});
+
+gulp.task('clear-build-folder', function(){
+	clean.sync(configCode.build_path);
+});
+
+gulp.task('default',['clear-build-folder'],function() {
+	gulp.start('copy-html');
+	elixir(function(mix){
+		mix.styles(configCode.vendor_path_css.concat([configCode.assets_path + '/css/**/*.css']),
+				'public/assets/css/all.css', configCode.assets_path);
+		mix.scripts(configCode.vendor_path_js.concat([configCode.assets_path + '/js/**/*.js']),
+				'public/assets/js/all.js', configCode.assets_path);
+		
+		// Copy FONTS
+	    mix.copy(configCode.bower_path + '/bootstrap/fonts', 'public/assets/fonts/')
+	    	.copy(configCode.bower_path + '/font-awesome/fonts', 'public/assets/fonts/');
+
+		mix.version(['/assets/css/all.css', '/assets/js/all.js']);
+	});
+});
+
+gulp.task('watch-dev',['clear-build-folder'], function(){
+	liveReload.listen();
+	gulp.start('copy-styles','copy-scripts', 'copy-html');
+	gulp.watch(configCode.assets_path + '/**', ['copy-styles','copy-scripts', 'copy-html']);
+});
+
---
+++ resources/views/app.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="app">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laravel</title>
+	
+	@if(Config::get('app.debug'))
+		<link rel="stylesheet" type="text/css" href="{{asset('build/assets/css/vendor/bootstrap.min.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{asset('build/assets/css/vendor/bootstrap-theme.min.css')}}">
+	@else
+		<link rel="stylesheet" type="text/css" href="{{elixir('assets/css/all.css')}}">
+	@endif
+
+	<!-- Fonts -->
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Laravel</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="{{ url('/') }}">Welcome</a></li>
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
+					@if(auth()->guest())
+						@if(!Request::is('auth/login'))
+							<li><a href="{{ url('/auth/login') }}">Login</a></li>
+						@endif
+						@if(!Request::is('auth/register'))
+							<li><a href="{{ url('/auth/register') }}">Register</a></li>
+						@endif
+					@else
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+							</ul>
+						</li>
+					@endif
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+<div ng-view></div>
+
+	<!-- Scripts -->
+	@if(Config::get('app.debug'))
+			<script type="text/javascript" src="{{asset('build/assets/js/vendor/jquery.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('build/assets/js/vendor/angular.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('build/assets/js/vendor/angular-route.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('build/assets/js/vendor/angular-resource.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('build/assets/js/vendor/angular-animate.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('build/assets/js/vendor/angular-messages.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('build/assets/js/vendor/ui-bootstrap.min.js')}}"></script>
+			<script type="text/javascript" src="{{asset('build/assets/js/vendor/navbar.min.js')}}"></script>
+
+			<script type="text/javascript" src="{{asset('build/assets/js/app.js')}}"></script>
+	@else
+		<script type="text/javascript" src="{{elixir('assets/js/all.js')}}"></script>
+	@endif
+</body>
+</html>
